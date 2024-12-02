35415782ece30f17067406b43284306128e8e596
ErnandoM <ernandomanoel.freire@gmail.com>
2024-12-02 11:18:24 -0300

    added the Home screen and your styles

---
+++ src/app/index/index.tsx
@@ -0,0 +1,141 @@
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  Text,
+  Alert,
+  Linking,
+} from "react-native";
+import { styles } from "./styles";
+
+import { router, useFocusEffect } from "expo-router";
+
+import { MaterialIcons } from "@expo/vector-icons";
+import { colors } from "@/styles/colors";
+import { Link } from "@/components/link";
+import { Categories } from "@/components/categories";
+import { Option } from "@/components/option";
+import { useCallback, useState } from "react";
+import { categories } from "@/utils/categories";
+import { LinkStorage, linkStorage } from "@/storage/link-storage";
+
+export default function Index() {
+  const [showModal, setShowModal] = useState(false);
+  const [link, setLink] = useState<LinkStorage>({} as LinkStorage);
+  const [category, setCategory] = useState(categories[0].name);
+  const [links, setLinks] = useState<LinkStorage[]>([]);
+
+  async function getLinks() {
+    try {
+      const response = await linkStorage.get();
+
+      const filtered = response.filter((link) => link.category === category);
+
+      setLinks(filtered);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possivel listar os links");
+    }
+  }
+
+  function handleDetails(selected: LinkStorage) {
+    setShowModal(true);
+    setLink(selected);
+  }
+
+  async function linkRemove() {
+    try {
+      await linkStorage.remove(link.id);
+      getLinks();
+      setShowModal(false);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possivel excluir");
+      console.log(error);
+    }
+  }
+
+  function handleRemove() {
+    Alert.alert("Excluir", "Deseja realmente excluir?", [
+      { style: "cancel", text: "Não" },
+      { text: "Sim", onPress: linkRemove },
+    ]);
+  }
+
+  async function handleLinkOpen() {
+    try {
+      await Linking.openURL(link.url);
+      setShowModal(false);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possivel abrir o link");
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      getLinks();
+    }, [category])
+  );
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+        <TouchableOpacity onPress={() => router.navigate("/add")}>
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
+      <Categories selected={category} onChange={setCategory} />
+
+      {links.length == 0 ? (
+        <View style={styles.emptyLinks}>
+          <Text style={styles.emptyLinksLabel}>Não tem links adicionado</Text>
+        </View>
+      ) : (
+        <FlatList
+          data={links}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Link
+              name={item.name}
+              url={item.url}
+              onDetail={() => handleDetails(item)}
+            />
+          )}
+          style={styles.links}
+          contentContainerStyle={styles.linksContent}
+          showsVerticalScrollIndicator={false}
+        />
+      )}
+
+      <Modal visible={showModal} transparent animationType="slide">
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>{link.category}</Text>
+              <TouchableOpacity onPress={() => setShowModal(false)}>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.modalLinkName}>{link.name}</Text>
+            <Text style={styles.modalUrl}>{link.url}</Text>
+
+            <View style={styles.modalFooter}>
+              <Option
+                name="Excluir"
+                icon="delete"
+                variant="secondary"
+                onPress={handleRemove}
+              />
+              <Option name="Abrir" icon="language" onPress={handleLinkOpen} />
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+}
---
+++ src/app/index/styles.ts
@@ -0,0 +1,89 @@
+import { StyleSheet } from "react-native";
+import { colors } from "@/styles/colors";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 14,
+  },
+
+  title: { color: colors.green[900], fontSize: 22 },
+
+  header: {
+    paddingHorizontal: 24,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+
+  logo: {
+    height: 32,
+    width: 38,
+  },
+
+  links: {
+    borderTopWidth: 1,
+    borderTopColor: colors.gray[600],
+  },
+  linksContent: {
+    gap: 20,
+    padding: 24,
+    paddingBottom: 100,
+  },
+
+  emptyLinks: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  emptyLinksLabel: {
+    fontSize: 20,
+    color: colors.gray[300],
+  },
+
+  modal: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  modalContent: {
+    backgroundColor: colors.gray[900],
+    borderTopWidth: 1,
+    borderTopColor: colors.gray[800],
+    paddingBottom: 32,
+    padding: 24,
+  },
+  modalHeader: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 42,
+  },
+  modalCategory: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: "500",
+    color: colors.gray[400],
+  },
+  modalLinkName: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: colors.gray[200],
+  },
+  modalUrl: {
+    fontSize: 14,
+    color: colors.gray[400],
+  },
+
+  modalFooter: {
+    flexDirection: "row",
+    marginTop: 32,
+    width: "100%",
+    justifyContent: "space-between",
+    borderTopWidth: 1,
+    borderTopColor: colors.gray[600],
+    paddingVertical: 14,
+  },
+});
